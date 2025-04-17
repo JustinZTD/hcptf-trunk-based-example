356746fabe912f5bf277e7aaf90cb0ff4520b3c9
Justin <127295157+JustinZTD@users.noreply.github.com>
2025-04-17 17:00:53 +0900

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -24,3 +24,8 @@ resource "random_pet" "example_three" {
   prefix    = var.pet_prefix
   separator = var.pet_separator
 }
+resource "random_pet" "example_4" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
